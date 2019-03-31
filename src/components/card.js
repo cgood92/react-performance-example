d530f102feb5b41837308d196f933ffb47931f6f
Clint Goodman <clintgoodman27@gmail.com>
2019-03-30 19:04:13 -0600

    step 4: Cleanup debugging

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
 import { select } from "../store/selected";
@@ -9,35 +9,15 @@ import { isSelected, getItem } from "../store/selectors";
 
 const getImage = name => `https://api.adorable.io/avatars/200/${name}.png`;
 
-class Card extends Component {
-  shouldComponentUpdate(nextProps) {
-    console.group();
-    Object.keys(nextProps).forEach(key => {
-      if (nextProps[key] !== this.props[key]) {
-        console.log(`${key} has changed...`);
-        console.log("old:", this.props[key]);
-        console.log("new:", nextProps[key]);
-        console.log(
-          "do objects look the same:",
-          JSON.stringify(this.props[key]) === JSON.stringify(nextProps[key])
-        );
-      }
-    });
-    console.groupEnd();
-    return true;
-  }
-  render() {
-    const { id, isSelected, item, select } = this.props;
-    const { name } = item;
-    console.count(`${name} is rendered`);
-    return (
-      <CardContainer onClick={() => select(id)} selected={isSelected}>
-        <Image src={getImage(name)} />
-        <Caption>{name}</Caption>
-      </CardContainer>
-    );
-  }
-}
+const Card = ({ id, isSelected, item, select }) => {
+  const { name } = item;
+  return (
+    <CardContainer onClick={() => select(id)} selected={isSelected}>
+      <Image src={getImage(name)} />
+      <Caption>{name}</Caption>
+    </CardContainer>
+  );
+};
 
 const mapStateToProps = (state, { id }) => ({
   isSelected: isSelected(state, id),
